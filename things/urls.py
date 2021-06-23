3c74de5b9df6f22706776fc563408835689c9c8d
Mason Aviles <mce.aviles@gmail.com>
2021-06-23 03:02:33 -0700

    initial set up

---
+++ things/urls.py
@@ -0,0 +1,10 @@
+# url patterns go here
+
+from django.urls import path
+
+from .views import AboutPageView, HomePageView
+
+urlpatterns = [
+    path("", HomePageView.as_view(), name="home"),
+    path("about/", AboutPageView.as_view(), name="about"),
+]
